3cacd9a0f539fbe75a7731b7269d25a8f7ff53b8
Oliver Oxenham <ooxenham@stubhub.com>
2014-08-31 11:31:05 -0700

    progress component updated

--- src/scripts/components/progress_component.js
+++ src/scripts/components/progress_component.js
@@ -6,7 +6,20 @@ var React = require('react');
 
 var ProgressComponent = React.createClass({
     render: function() {
-        return <div className="progress_component">progress bar</div>
+        var circles = [];
+        for (var i=-1; i<this.props.length; i++) {
+            if (this.props.position === i) {
+                circles.push(<div key={i} className="progress_circle full"></div>);
+            }
+            else {
+                circles.push(<div key={i} className="progress_circle"></div>);
+            }
+        }
+        return (
+            <div className="progress_component">
+                {circles}
+            </div>
+        );
     }
 });
 
